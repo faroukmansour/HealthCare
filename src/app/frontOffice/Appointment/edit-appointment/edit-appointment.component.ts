40614d40e9ff078bb568a9dac57e9a26a57dae50
faroukmansour <faroukmansour@esprit.tn>
2023-05-09 18:34:21 +0200

    farouk

--- src/app/frontOffice/Appointment/edit-appointment/edit-appointment.component.ts
+++ src/app/frontOffice/Appointment/edit-appointment/edit-appointment.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { log } from 'console';
 import { ToastrService } from 'ngx-toastr';
 import { AppointmentService } from 'src/app/shared/appointment.service';
 import { Appointment } from 'src/models/Appointment';
